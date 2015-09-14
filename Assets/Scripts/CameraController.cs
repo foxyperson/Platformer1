8c4640922dee67ae701f604894ed3ff83f6bc617
Oskar Therén <oskartheren@gmail.com>
2015-09-14 17:39:45 +0200

    First upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	private Vector2 playerRelativeToEdge;
+
+	public float cameraLowestY;
+	private float offsetCameraLowestY;
+	public float cameraLowestX;
+	private float offsetCameraLowestX;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+
+		Camera cam = Camera.main;
+		offsetCameraLowestY = cameraLowestY + cam.orthographicSize;
+		offsetCameraLowestX = cameraLowestX + cam.orthographicSize * cam.aspect;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+		if (transform.position.y <= offsetCameraLowestY) {
+			transform.position = new Vector3(transform.position.x, offsetCameraLowestY, transform.position.z);
+		} 
+		if (transform.position.x <= offsetCameraLowestX) {
+			transform.position = new Vector3(offsetCameraLowestX, transform.position.y, transform.position.z);
+		} 
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float moveX;
+	public float moveVelocity;
+
+	private float jump;
+	public float jumpVelocity;
+
+	private bool grounded;
+	private bool hasDoubleJumped = false;
+
+	public int deathDepth;
+	public Vector3 respawnPos;
+	int jumpCounter = 0;
+
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}*/
+	
+	// Update is called once per frame
+	void Update () {	
+		// Jump
+		
+
+		if (grounded) {
+			hasDoubleJumped = false;
+			if (Input.GetKey(KeyCode.Space)) {
+				jumpCounter++;
+				jump = jumpVelocity;
+			}
+			else
+				jump = 0;
+		} else {
+			if (Input.GetKeyDown(KeyCode.Space) && !(hasDoubleJumped)){
+				jumpCounter++;
+				jump = jumpVelocity;
+				hasDoubleJumped = true;
+			}
+			else
+				jump = GetComponent<Rigidbody>().velocity.y;
+		}
+
+		// Movement
+		moveX = Input.GetAxis("Horizontal") * moveVelocity;
+
+		GetComponent<Rigidbody>().velocity = new Vector3(moveX, jump, 0);
+
+		// Death
+		if (this.transform.position.y < deathDepth) {
+			this.transform.position = respawnPos;
+		}
+	
+	}
+	// Grounded
+	void OnTriggerEnter() {
+		grounded = true;
+	}
+	void OnTriggerExit() {
+		grounded = false;
+	}
+}
