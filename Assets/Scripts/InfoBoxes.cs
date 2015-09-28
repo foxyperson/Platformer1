5fb494d7d05d4ccf9f0e8268c4b4ab1c34c7c01c
Oskar Therén <oskartheren@gmail.com>
2015-09-28 10:01:48 +0200

    started working on textboxes

---
+++ Assets/Scripts/InfoBoxes.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoBoxes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GUI.TextArea(new Rect(0, 0, 0, 0), "Hello world!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
