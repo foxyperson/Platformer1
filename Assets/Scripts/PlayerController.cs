4583f842d9e3f8f2daec95854b258295272cb88c
Oskar Therén <oskartheren@gmail.com>
2015-10-07 11:52:03 +0200

    Changed the map a bit, and made a script to change from ortographic view

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,9 +16,9 @@ public class PlayerController : MonoBehaviour {
 	public int deathDepth;
 	public Vector3 respawnPos;
 
-	private bool hasDoubleJump = false;
-	private bool hasDepthWalk = false;
-	private bool hasControlCamera = false;
+	private bool hasDoubleJump = true;
+	private bool hasDepthWalk = true;
+	private bool hasControlCamera = true;
 
     private float rotationAmount = 90;
     private float currentRotation = 0;
---
+++ Assets/Scripts/SwitchTo3D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchTo3D : MonoBehaviour {
+
+	public Camera cam;
+	public GameObject level;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			Camera.main.orthographic = false;
+		}
+	}
+}
